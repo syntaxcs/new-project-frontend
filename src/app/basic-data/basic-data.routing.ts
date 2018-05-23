de8ff7cf37a4d46354ea373c46b28cab9ee75dff
Patinya14 <57160342@go.buu.ac.th>
2018-05-23 14:42:05 +0700

    remedy

--- src/app/basic-data/basic-data.routing.ts
+++ src/app/basic-data/basic-data.routing.ts
@@ -1,17 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
-
-import { BasicDataComponent } from './basic-data.component'
-import { DiseaseComponent } from './disease/disease.component'
-import { TreaterComponent } from './treater/treater.component'
-<<<<<<< HEAD
-import { RemedyComponent } from './remedy/remedy.component'
-=======
+import { BasicDataComponent } from './basic-data.component';
+import { DiseaseComponent } from './disease/disease.component';
+import { TreaterComponent } from './treater/treater.component';
+import { RemedyComponent } from './remedy/remedy.component';
 import { DrugComponent } from './drug/drug.component';
 import { FollowComponent } from './follow/follow.component';
 import { PersonalComponent } from './personal/personal.component';
-
->>>>>>> 78f26e21abf12c14d0366712b5fbc907959789f9
 
 export const routes: Routes = [
     {
@@ -27,11 +22,11 @@ export const routes: Routes = [
         component: TreaterComponent
     },
     {
-<<<<<<< HEAD
+
         path: 'remedy',
         component: RemedyComponent
     },
-=======
+    {
         path: 'drug',
         component: DrugComponent
     },
@@ -40,10 +35,10 @@ export const routes: Routes = [
         component: FollowComponent
     },
     {
-        path: 'personal',
+    path: 'personal',
         component: PersonalComponent
     },
     
->>>>>>> 78f26e21abf12c14d0366712b5fbc907959789f9
+
 ];
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
