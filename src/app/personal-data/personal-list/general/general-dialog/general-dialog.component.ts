df03b987100ba0f7eeecb518cc12c9ebf3a438cf
syntaxcs <syntax_cs@hotmail.com>
2018-05-26 21:38:27 +0700

    fix year

--- src/app/personal-data/personal-list/general/general-dialog/general-dialog.component.ts
+++ src/app/personal-data/personal-list/general/general-dialog/general-dialog.component.ts
@@ -9,13 +9,22 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class GeneralDialogComponent implements OnInit {
   public form: FormGroup;
+  startDate: any;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private formBuilder: FormBuilder,
     private dialogRef: MatDialogRef<GeneralDialogComponent>,
   ) { }
+
   ngOnInit() {
     this.form = this.formBuilder.group({});
+    this.calculateYear();
+  }
+  calculateYear() {
+    let year = new Date().getFullYear() + 543;
+    let month = new Date().getMonth()
+    this.startDate = new Date(year+1, month+1, null, null, null, null);
   }
   onClose() {
     this.dialogRef.close(/*sent value to tab-supervision*/);
